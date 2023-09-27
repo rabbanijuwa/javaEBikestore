918923b4ed5f0acd539c53ebc7ab41aa4e64a23a
juwarabbani <rabbanijuwa@gmail.com>
2023-09-27 11:04:48 -0400

    EBike storefront

---
+++ Product.java
@@ -0,0 +1,26 @@
+public class Product {
+    private int id;
+    private String productName;
+    private String description;
+    private double price;
+    private int stockQuantity;
+    public Product(int id, String productName, String description, double price, int stockQuantity){
+        this.id = id;
+        this.productName = productName;
+        this.description = description;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public String getProductName(){
+        return productName;
+    }
+    public void
+
+}
+
